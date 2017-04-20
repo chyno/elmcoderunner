76283d09fcb263324592b6750e1814b13f978a21
chyno <jchynowe1@yahoo.com>
2017-04-19 22:23:35 -0400

    add table

--- main.elm
+++ main.elm
@@ -9,10 +9,9 @@ update msg model =
      case msg of
         NoOp ->
             model ! []
-
         Add ->
           { model
-                | kata = Just {id = -1, name="", code = "", test = ""}
+                | kata = Just defaultkata
           }
           ! []
         Edit kt ->
@@ -20,19 +19,48 @@ update msg model =
                 | kata = Just kt
           }
           ! []
-           
-
-
-  
+        Save  kt ->
+          { model
+                | kata = Nothing,
+                katas = (updateKata model.katas kt)
+          }
+          ! []
+        Cancel   ->
+          { model
+                | kata = Nothing
+                
+          }
+          ! []
+        SetTest kt txt ->
+          { model
+                | kata = Just (Kata kt.id kt.name kt.code txt)
+          }
+          ! []
+             
 init :  ( Model, Cmd Msg )
 init =
   defaultModel  ! []
 
     
 main =
-  Html.program  
+  Html.program    
   { init = init
   , view = view
   , update = update
   , subscriptions = \_ -> Sub.none
   }
+
+
+updateKata : List Kata -> Kata -> List Kata
+updateKata katas updatedKata =
+    let
+        pick currentKata =
+            if updatedKata.id == currentKata.id then
+                updatedKata
+            else
+                currentKata
+
+        updateList players =
+            List.map pick players     
+    in
+      updateList katas
