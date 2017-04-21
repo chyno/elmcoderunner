f0aeddc09cc0086bf8a0a8db0ede685dd0e4e8aa
chyno <jchynowe1@yahoo.com>
2017-04-21 11:27:54 -0400

    view components components

---
+++ src/views/static.elm
@@ -0,0 +1,10 @@
+module Views.Static exposing (..)
+
+import Dom
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+header  :   Html a
+header  =
+ div [] [text "this is the header"]
